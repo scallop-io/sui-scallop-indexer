a8e31126860f81815d43c22b3540466065a2673a
Donnie <donor.gm@gmail.com>
2023-09-07 05:27:14 +0800

    build: add alias path for new folders

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    moduleNameMapper: {
+      '^@modules/(.*)$': '<rootDir>/src/modules/$1',
+      '^@services/(.*)$': '<rootDir>/src/services/$1',
+      '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    },
+};
